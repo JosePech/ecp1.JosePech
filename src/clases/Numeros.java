48cd6defb3a42e2d8173e1c68552b37fde351101
JosePech <jrocks169@gmail.com>
2014-10-03 12:42:42 +0200

    CR1.03

--- src/clases/Numeros.java
+++ src/clases/Numeros.java
@@ -10,7 +10,7 @@ public class Numeros {
         return a - b;
     }
     
-    public double multiplica(double a, double b){
+    public static double multiplica(double a, double b){
         return a * b;
     }
 
