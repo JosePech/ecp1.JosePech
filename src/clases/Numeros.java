c229f77809379dc3016e3840dd6433ca9312109b
JosePech <jrocks169@gmail.com>
2014-10-03 12:06:38 +0200

    CD2

--- src/clases/Numeros.java
+++ src/clases/Numeros.java
@@ -5,5 +5,9 @@ public class Numeros {
     public double suma(double a, double b){
         return a + b;
     }
+    
+    public double resta(double a, double b){
+        return a - b;
+    }
 
 }
