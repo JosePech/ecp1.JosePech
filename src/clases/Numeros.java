21cc39d1cdef387e647ff021d5fa8933b931a3e4
JosePech <jrocks169@gmail.com>
2014-10-03 12:11:17 +0200

    CD4

--- src/clases/Numeros.java
+++ src/clases/Numeros.java
@@ -13,5 +13,9 @@ public class Numeros {
     public double multiplica(double a, double b){
         return a * b;
     }
+    
+    public double divide(double a, double b){
+        return a/b;
+    }
 
 }
